be20057fdb7c804eab28031db4460e4f9de672e7
emmanuelmeena91 <emmanuel.meena@moringaschool.com>
2023-06-29 12:25:40 +0300

    complete arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+
+let multiply;
+let num1 = 31;
+let num2 = 2;
+
+multiply = num1 * num2;
+
+console.log(multiply);
+
+let random = Math.floor(Math.random() * (Number.MAX_SAFE_INTEGER - 1)) + 1;
+
+let mod;
+let num3 = 14;
+let num4 = 5;
+ mod = num3 % num4;
+
+ console.log(mod);//is equal to 4
+
+ let set = [2, 15, 10, 20, 6, 1];
+let max = Math.max(...set); // max is now equal to 20
